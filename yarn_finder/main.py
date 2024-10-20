217ea9528370038fa42798d86fc459a5f5795b3c
Cerek Hillen <cerekh@gmail.com>
2024-10-20 12:38:14 -0700

    once again improve deployment strategy

--- yarn_finder/main.py
+++ yarn_finder/main.py
@@ -5,12 +5,21 @@ import uvicorn
 
 
 def main() -> None:
-    uvicorn.run(
-        "yarn_finder.wsgi:app",
-        host=os.environ.get("HOST", "127.0.0.1"),
-        port=int(os.environ.get("PORT", "8080")),
-        reload=sys.platform == "darwin",
-    )
+    profile = os.environ.get("PROFILE", "dev")
+    match profile:
+        case "dev":
+            uvicorn.run(
+                "yarn_finder.wsgi:app",
+                host="127.0.0.1",
+                port=8080,
+                reload=sys.platform == "darwin",
+            )
+
+        case "prod":
+            uvicorn.run(
+                "yarn_finder.wsgi:app",
+                uds="/tmp/uvicorn.sock",
+            )
 
 
 if __name__ == "__main__":

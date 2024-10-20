c1bc78cfc2fd5bd583d6d01dd04370bc5cc8cbcf
Cerek Hillen <cerekh@gmail.com>
2024-10-20 12:27:32 -0700

    make it so i can host to the web

--- yarn_finder/main.py
+++ yarn_finder/main.py
@@ -1,12 +1,15 @@
+import os
+import sys
+
 import uvicorn
 
 
 def main() -> None:
     uvicorn.run(
         "yarn_finder.wsgi:app",
-        host="127.0.0.1",
-        port=8080,
-        reload=True,
+        host=os.environ.get("HOST", "127.0.0.1"),
+        port=int(os.environ.get("PORT", "8080")),
+        reload=sys.platform == "darwin",
     )
 
 
